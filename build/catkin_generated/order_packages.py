8865d45e73f1556680f2385fe99f6b6b21fa101f
albert0216 <hunter.albert25@gmail.com>
2021-08-20 15:05:21 +0800

    aa

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/aa/test/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/aa/test/devel;/opt/ros/noetic'.split(';') if '/home/aa/test/devel;/opt/ros/noetic' != '' else []
---
+++ build/example/CMakeFiles/aa.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/aa/test/devel/lib/example/aa"
+  "/home/aa/test/devel/lib/example/aa.pdb"
+  "CMakeFiles/aa.dir/src/hello_world.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
